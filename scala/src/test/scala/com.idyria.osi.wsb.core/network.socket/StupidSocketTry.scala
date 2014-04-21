a169d581fa9dbb5a4cca6787f8f6b990280a2700
rleys <richard.leys@ziti.uni-heidelberg.de>
2014-04-21 16:09:14 +0200

    Updated licenses

--- scala/src/test/scala/com.idyria.osi.wsb.core/network.socket/StupidSocketTry.scala
+++ scala/src/test/scala/com.idyria.osi.wsb.core/network.socket/StupidSocketTry.scala
@@ -83,6 +83,28 @@ class StupidSocketTry extends FlatSpec with GivenWhenThen {
             And("Port is resusable")
             println("Rebinding")
             var serverSocket = ServerSocketChannel.open();
+
+/*
+ * #%L
+ * WSB Core
+ * %%
+ * Copyright (C) 2008 - 2014 OSI / Computer Architecture Group @ Uni. Heidelberg
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
             serverSocket.bind(new InetSocketAddress(address,port))
             serverSocket.close
     }
