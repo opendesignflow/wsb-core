bb999db9ba357feaacb61190a1343226b821f43a
richnou <leys.richard@gmail.com>
2017-06-12 14:41:00 +0200

    Updated licensing and to newest version of POM Configs

--- scala/src/main/scala/com/idyria/osi/wsb/core/network/connectors/tcp/TCPConnector.scala
+++ scala/src/main/scala/com/idyria/osi/wsb/core/network/connectors/tcp/TCPConnector.scala
@@ -130,6 +130,27 @@ abstract class TCPConnector extends AbstractConnector[TCPNetworkContext] with Li
             case e: IOException =>
               // e.printStackTrace()
               logFine[TCPConnector]("Error on send, sending event close -> " + e.getLocalizedMessage);
+
+/*-
+ * #%L
+ * WSB Core
+ * %%
+ * Copyright (C) 2008 - 2017 Open Design Flow
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
               //context.@->("close")
               //resBuffer.clear()
               try {
--- scala/src/main/scala/com/idyria/osi/wsb/lib/simplerpc/SimpleRPCMessage.scala
+++ scala/src/main/scala/com/idyria/osi/wsb/lib/simplerpc/SimpleRPCMessage.scala
@@ -96,6 +96,27 @@ object SimpleRPCMessage extends MessageFactory {
     
     var str = new String(data.asInstanceOf[ByteBuffer].array())
     println(s"Message: $str");
+
+/*-
+ * #%L
+ * WSB Core
+ * %%
+ * Copyright (C) 2008 - 2017 Open Design Flow
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
     
     // Parse XML
     var any = AnyXList()
